b6691c580cfb904f5868eba57860c2b5b9bdb7c2
Egor Schastlivtsev <eschastlivtsev@klika-tech.com>
2024-11-22 17:46:12 +0500

    feat(devops): create fw in iot connect

--- iot-connect/constants.py
+++ iot-connect/constants.py
@@ -5,21 +5,25 @@ STATUS_OK = 200
 API_AUTH_URL = "https://awspocauth.iotconnect.io/api/v2.1/"
 API_DEVICE_URL = "https://awspocdevice.iotconnect.io/api/v2.1/"
 API_USER_URL = "https://awspocuser.iotconnect.io/api/v2.1/"
+API_FW_URL = "https://awspocfirmware.iotconnect.io/api/v2.1/"
 
 BASIC_TOKEN = "Auth/basic-token"
 LOGIN = "Auth/login"
-CREATE_DEVICE_TEMPLATE = "device-template/quick"
+DEVICE_TEMPLATE_CREATE = "device-template/quick"
 DEVICE_TEMPLATE_LIST = "device-template"
-CREATE_DEVICE = "Device"
+DEVICE_CREATE = "Device"
 ENTITY_LIST = "Entity"
+FW_ADD = "Firmware"
 
 DEVICE_TEMPLATES_DIR = "templates/devices/"
 TEMPLATES_TAIL = "_template.JSON"
 
-DEVICE_WITH_OWN_CERT = "soundclass"
-DEVICE_WITH_AWS_CERT = "soundgener"
+DEVICE_WITH_SOUND_CLASS = "soundclass"
+DEVICE_WITH_SOUND_GEN = "soundgener"
 
-DEVICE_TEMPLATES = [DEVICE_WITH_OWN_CERT, DEVICE_WITH_AWS_CERT]
+DEVICE_TEMPLATES = [DEVICE_WITH_SOUND_CLASS, DEVICE_WITH_SOUND_GEN]
 
 DEVICE_TEMPLATE_ALREADY_EXISTS = "DeviceTemplateNameAlreadyExists"
 DEVICE_ALREADY_EXISTS = "UniqueIdAlreadyExists"
+
+FW_OTA_FILE = "ota/fw.bin"
---
+++ iot-connect/iot_connect_fw_ota.py
@@ -0,0 +1,80 @@
+"""This module performs partial IoTConnect OTA FW Update."""
+
+import argparse
+import requests
+
+from authentication import authenticate
+from constants import (
+    DEVICE_TEMPLATES_DIR,
+    TEMPLATES_TAIL,
+    API_DEVICE_URL,
+    DEVICE_TEMPLATE_CREATE,
+    DEVICE_TEMPLATE_LIST,
+    DEVICE_TEMPLATE_ALREADY_EXISTS,
+    DEVICE_ALREADY_EXISTS,
+    DEVICE_WITH_SOUND_GEN,
+    DEVICE_WITH_SOUND_CLASS,
+    DEVICE_CREATE,
+    ENTITY_LIST,
+    API_USER_URL,
+    FW_OTA_FILE,
+    API_FW_URL,
+    FW_ADD
+)
+from check_status import check_status, BadHttpStatusException
+from common import (
+    get_template_guid,
+    get_entity_guid
+)
+
+
+def iot_connect_fw_ota():
+    """Perform IoTConnect FW OTA"""
+    args = parse_arguments()
+    access_token = authenticate(args)
+    print("Successful login - now create OTA update for the device.")
+    template_guid = get_template_guid(DEVICE_WITH_SOUND_CLASS, access_token)
+    create_firmware(template_guid, access_token, "1.1.99")
+    # entity_guid = get_entity_guid(args.entity_name, access_token)
+
+def parse_arguments() -> argparse.Namespace:
+    """
+    Parse CLI arguments - 4 obligatory positional argiments
+     - username
+     - password
+     - solution key
+     - entity name
+    """
+    print("Parse command line arguments")
+    parser=argparse.ArgumentParser()
+    parser.add_argument("username")
+    parser.add_argument("password")
+    parser.add_argument("solution_key")
+    parser.add_argument("entity_name")
+    return parser.parse_args()
+
+def create_firmware(device_template_guid: str, access_token: str, fw_version: str) -> str:
+    """Create new firmware in IoTConnect"""
+    data = {
+        "deviceTemplateGuid": device_template_guid,
+        "software": fw_version,
+        "firmwareName": "DEVICE_WITH_SOUND_CLASS_" + fw_version,
+        "hardware": "1.0.0"
+    }
+
+    headers = {
+        "Content-type": "application/json",
+        "Accept": "*/*",
+        "Authorization": access_token
+    }
+    response = requests.post(API_FW_URL + FW_ADD, json = data, headers = headers)
+    check_status(response)
+    response_json = response.json()
+    guid = response_json["data"][0]["firmwareUpgradeGuid"]
+
+    print(f"Firmware {data["firmwareName"]} created with guid {guid}")
+    return guid
+
+
+if __name__ == "__main__":
+    iot_connect_fw_ota()
