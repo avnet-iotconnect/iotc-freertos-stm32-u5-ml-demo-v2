cf3ec548a25bbffec191cc9f84061caf9346578e
klisevich <klisevch@klika-tech.com>
2024-11-26 08:29:28 +0100

    Added comments to a header file Added return codes

--- stm32/Projects/Common/app/s3_client/s3_https_client.h
+++ stm32/Projects/Common/app/s3_client/s3_https_client.h
@@ -1,45 +1,85 @@
 #ifndef S3_HTTPS_CLIENT_H
 #define S3_HTTPS_CLIENT_H
 
-#include "FreeRTOS.h"
+/**
+ * @file S3HttpsClient.h
+ * @brief Provides functions for interacting with the S3 HTTPS client.
+ */
+
+#include "FreeRTOS.h"   /**< FreeRTOS includes for task management and types */
+
+/* ============================ Return Codes ============================ */
+
+/** @brief Success code for operations */
+#define S3_CLIENT_SUCCESS        0   /**< Operation was successful */
+
+/** @brief General error code */
+#define S3_CLIENT_ERROR          -1  /**< General error */
+
+/** @brief TLS-related errors */
+#define S3_CLIENT_TLS_ERROR      -2  /**< Error in TLS setup or connection */
+
+/** @brief HTTP-related errors */
+#define S3_CLIENT_HTTP_ERROR     -3  /**< Error in HTTP request or response */
+
+/** @brief Network-related errors */
+#define S3_CLIENT_NETWORK_ERROR  -4  /**< Error in establishing or maintaining network connection */
+
+/** @brief Input parameters related errors */
+#define S3_CLIENT_INVALID_PARAM  -5  /**< Error processing input parameters*/
+
 
 /**
  * @brief Initializes the S3 client module.
+ * 
+ * This function initializes any resources required by the S3 client and prepares
+ * the module for establishing connections with the S3.
+ * 
+ * @return S3_CLIENT_SUCCESS if initialization was successful, 
+ *         S3_CLIENT_ERROR if there was an error.
  */
-void S3Client_Init(void);
+int S3Client_Init(void);
 
 /**
  * @brief Establishes an HTTPS connection to the S3 endpoint.
- *
- * @return pdTRUE if the connection was successful, otherwise pdFALSE.
+ * 
+ * This function attempts to connect to the S3 endpoint over HTTPS. 
+ * It returns S3_CLIENT_SUCCESS if the connection was successfully established, 
+ * or an error code if the connection failed.
+ * 
+ * @return S3_CLIENT_SUCCESS if the connection was successful, 
+ *         S3_CLIENT_CONN_FAILED if the connection failed.
  */
-BaseType_t S3Client_Connect(void);
+int S3Client_Connect(void);
 
 /**
- * @brief Sends a PUT request to upload an object to S3.
+ * @brief Sends a POST request to upload an object to AWS S3.
+ * 
+ * This function sends a POST request to the S3 endpoint to upload an object.
  *
- * @param[in] payload Pointer to the data to be uploaded.
- * @param[in] payloadLength Length of the data in bytes.
- *
- * @return pdTRUE if the PUT request was successful, otherwise pdFALSE.
+ * @return S3_CLIENT_SUCCESS on success, or appropriate error code on failure.
  */
-BaseType_t S3Client_PutObject(const char *payload, uint32_t payloadLength);
+int S3Client_PostObject(const char *payload, uint32_t payloadLength);
 
-/**
- * @brief Sends a GET request to download an object from S3.
- *
- * @return Pointer to the downloaded data on success, or NULL on failure.
- */
-const char *S3Client_GetObject(void);
 
 /**
  * @brief Closes the connection to the S3 endpoint.
+ * 
+ * This function closes the established HTTPS connection to the S3 endpoint.
+ * 
+ * @return S3_CLIENT_SUCCESS if the disconnection was successful, 
+ *         S3_CLIENT_DISCONNECT_FAILED if the disconnection failed.
  */
-void S3Client_Disconnect(void);
+int S3Client_Disconnect(void);
 
 /**
- * @brief Task function to test S3 functionality
+ * @brief Task function to test S3 functionality.
+ * 
+ * This FreeRTOS task function is used to test the S3 client operations such as 
+ * establishing a connection, uploading and downloading objects, and disconnecting.
+ * 
+ * @param pvParameters Parameters passed to the task.
  */
-void vS3ConnectTask( void * pvParameters );
+void vS3ConnectTask(void *pvParameters);
 
-#endif /* S3_CLIENT_H */
+#endif /* S3_HTTPS_CLIENT_H */
