9c59d795e56aa3389eb5fa7e84635499a10d7e79
Egor Schastlivtsev <eschastlivtsev@klika-tech.com>
2024-11-28 14:44:00 +0500

    feat(back): creds provider add sqs between api and lambda test6

--- demo-cdk/lib/creds-provider/creds-provider.ts
+++ demo-cdk/lib/creds-provider/creds-provider.ts
@@ -71,16 +71,19 @@ export class CredsProviderConstruct extends Construct {
                 'integration.request.header.Content-Type': `'application/x-www-form-urlencoded'`,
               },
               requestTemplates: {
-                'text/plain': `Action=SendMessage&MessageBody=$input.body`,
+                'application/json': 'Action=SendMessage&MessageBody=$input.body',
               },
               integrationResponses: [
                 {
-                  statusCode: "200",
-                  responseTemplates: {
-                    "application/json": `{"done": true}`,
-                  },
+                  statusCode: '200',
                 },
-              ],
+                {
+                  statusCode: '400',
+                },
+                {
+                  statusCode: '500',
+                }
+              ]
             },
           });
 
@@ -147,8 +150,14 @@ export class CredsProviderConstruct extends Construct {
 
         webhookResource.addMethod('POST', sqsIntegration, {
             methodResponses: [
+                {
+                    statusCode: '400',
+                },
                 { 
                     statusCode: '200',
+                },
+                {
+                    statusCode: '500',
                 }
             ]
         });
