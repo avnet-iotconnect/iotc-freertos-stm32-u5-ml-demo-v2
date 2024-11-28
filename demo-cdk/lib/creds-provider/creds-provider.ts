ae15dda45a7f65a0e7e6c178f00c45fb4963a907
Egor Schastlivtsev <eschastlivtsev@klika-tech.com>
2024-11-28 12:39:56 +0500

    feat(back): creds provider add sqs between api and lambda test1

--- demo-cdk/lib/creds-provider/creds-provider.ts
+++ demo-cdk/lib/creds-provider/creds-provider.ts
@@ -5,7 +5,9 @@ import {
     SecretValue,
     aws_apigateway,
     aws_iam,
-    aws_lambda
+    aws_lambda,
+    aws_sqs,
+    aws_lambda_event_sources
   } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { isProxy } from 'util/types';
@@ -18,6 +20,72 @@ export class CredsProviderConstruct extends Construct {
         const iotConnectPassword = this.node.tryGetContext('iotConnectPassword');
         const iotConnectSolutionKey = this.node.tryGetContext('iotConnectSolutionKey');
         const iotConnectEntity = this.node.tryGetContext('iotConnectEntity');
+
+        const apiQueue = new aws_sqs.Queue(
+            this, "ApigwSqsLambdaQueue",
+            {
+                visibilityTimeout: cdk.Duration.seconds(300)
+            }
+        );
+
+        const sqsEventSource = new aws_lambda_event_sources.SqsEventSource(apiQueue);
+
+        const apiGatewaySqsRole = new aws_iam.Role(this, 'ApiGatewaySqsRole', {
+            assumedBy: new aws_iam.ServicePrincipal('apigateway.amazonaws.com')
+        });
+
+        apiGatewaySqsRole.addManagedPolicy(
+            aws_iam.ManagedPolicy.fromManagedPolicyArn(
+                this, 'ApiGwPushCwPolicy', 'arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs'
+            )
+        )
+
+        apiGatewaySqsRole.attachInlinePolicy(
+            new aws_iam.Policy(this, 'apiGatewaySqsRoleInlinePolicy',
+                {
+                    statements: [
+                        new aws_iam.PolicyStatement({
+                            effect: aws_iam.Effect.ALLOW,
+                            actions: [
+                                'sqs:SendMessage',
+                                'sqs:ReceiveMessage',
+                                'sqs:PurgeQueue',
+                                'sqs:DeleteMessage',
+                            ],
+                            resources: [
+                                apiGatewaySqsRole.roleArn
+                            ]
+                        })
+                    ]
+                }
+            )
+        )
+
+        const sqsIntegration = new aws_apigateway.AwsIntegration({
+            service: 'sqs',
+            path: `${process.env.CDK_DEFAULT_ACCOUNT}/${apiQueue.queueName}`,
+            integrationHttpMethod: 'POST',
+            options: {
+              credentialsRole: apiGatewaySqsRole,
+              requestParameters: {
+                'integration.request.header.Content-Type': `'application/x-www-form-urlencoded'`,
+              },
+              requestTemplates: {
+                'application/json': 'Action=SendMessage&MessageBody=$input.body',
+              },
+              integrationResponses: [
+                {
+                  statusCode: '200',
+                },
+                {
+                  statusCode: '400',
+                },
+                {
+                  statusCode: '500',
+                }
+              ]
+            },
+          });
 
         const credsProviderLambdaRole = new aws_iam.Role(this, 'CredsProviderLambdaRole', {
             assumedBy: new aws_iam.ServicePrincipal('lambda.amazonaws.com')
@@ -59,6 +127,8 @@ export class CredsProviderConstruct extends Construct {
             layers: [credsProviderLayer]
         });
 
+        credsProviderLambda.addEventSource(sqsEventSource);
+
         // Create API Gateway for basic authentication
         const apiGateway = new aws_apigateway.RestApi(this, 'CredentialsWebhook', {
             restApiName: 'CretsWebhookApi',
@@ -67,15 +137,29 @@ export class CredsProviderConstruct extends Construct {
 
         const webhookResource = apiGateway.root.addResource('webhook');
         // Lambda integration for basic auth
-        const lambdaIntegration = new aws_apigateway.LambdaIntegration(
-            credsProviderLambda,
-            {
-                proxy: true
-            }
-        );
+        // const lambdaIntegration = new aws_apigateway.LambdaIntegration(
+        //     credsProviderLambda,
+        //     {
+        //         proxy: true
+        //     }
+        // );
 
-        webhookResource.addMethod('POST', lambdaIntegration, {
-            authorizationType: aws_apigateway.AuthorizationType.NONE,
+        // webhookResource.addMethod('POST', lambdaIntegration, {
+        //     authorizationType: aws_apigateway.AuthorizationType.NONE,
+        // });
+
+        webhookResource.addMethod('POST', sqsIntegration, {
+            methodResponses: [
+                {
+                    statusCode: '400',
+                },
+                { 
+                    statusCode: '200',
+                },
+                {
+                    statusCode: '500',
+                }
+            ]
         });
     }
 }
