bd0381e3e059fd21bd3ec9fcb8c651ac540ea1e3
Egor Schastlivtsev <eschastlivtsev@klika-tech.com>
2024-12-04 13:59:46 +0500

    feat(devops): codebuiild push code test 13

--- demo-cdk/lib/ml/sagemaker-pipeline.ts
+++ demo-cdk/lib/ml/sagemaker-pipeline.ts
@@ -228,8 +228,8 @@ export class SagmakerPipeline extends Construct {
                     'git config --global user.email "codebuild-ml"',
                     'ls -la',
                     'git checkout retrained-model || git checkout -b retrained-model',
-                    'rm -r /models/ml-source-ablrv/*',
-                    'aws s3 cp s3://${ML_OUTPUT_BUCKET}/ml/tmp/ml/ /models/ml-source-ablrv --recursive --quiet',
+                    'rm -r models/ml-source-ablrv/*',
+                    'aws s3 cp s3://${ML_OUTPUT_BUCKET}/ml/tmp/ml/ models/ml-source-ablrv --recursive --quiet',
                     'git add --all',
                     'git commit -m "retrained model"',
                     'git push -u origin retrained-model'
